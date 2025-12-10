76e384623889d43d002be3f07697209ec9676250
Michael Gorven <michael@gorven.net>
2025-12-10 11:33:38 -0800

    Use next() instead of list()[0]

--- entpy/entpy/gencode/introspection_generator.py
+++ entpy/entpy/gencode/introspection_generator.py
@@ -2,12 +2,12 @@ def generate(base_name: str) -> str:
     return f"""def _get_field(field_name: str) -> Field:
         schema = {base_name}Schema()
         fields = schema.get_all_fields()
-        field = list(
+        field = next(
             filter(
                 lambda field: field.name == field_name,
                 fields,
             )
-        )[0]
+        )
         if not field:
             raise ValueError(f"Unknown field: {{field_name}}")
         return field
